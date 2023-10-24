ac7fc7f15150c26eef2c9c4ed8ed1db86f6cab96
Carmen Morata <cmoratagarcia@gmail.com>
2023-10-24 09:42:31 -0700

    Add event listeners to number and operator keys

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const display = document.querySelector(".display");
-const clearBtn = document.querySelector(".clear-btn");
-const deleteBtn = document.querySelector(".del-btn");
-const button = document.querySelectorAll(".button");
+const clearBtn = document.querySelector("#clear-btn");
+const deleteBtn = document.querySelector("#del-btn");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+display.textContent = "0";
+//add event listener to each button
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {});
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {});
+});
+
+//take inner text
+//after each press, update display
+//asign to firstnum, operator, and secondnum
+//return result at fourth press
 
 function operate(firstNum, operator, secondNum) {
   switch (operator) {

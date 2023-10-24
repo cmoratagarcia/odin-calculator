65f56428b90cb8386456657d22b253e61e62d5db
Carmen Morata <cmoratagarcia@gmail.com>
2023-10-24 10:35:50 -0700

    Update display with symbol Initialize all three values Add contitional statement to number event listener

--- script.js
+++ script.js
@@ -5,26 +5,42 @@ const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 
 display.textContent = "";
+
+let firstNum = 0;
+let secondNum = 0;
+let symbol = "";
 //add event listener to each button
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
+    //Add a max length
+
     //take inner text
     //after each press, update display
     let value = number.textContent;
     display.textContent += value;
+    //If operator hasn't been entered, store as first value
+    if ((symbol = "")) {
+      firstNum = value;
+    } else {
+      secondNum = value;
+    }
   });
 });
 
 operators.forEach((operator) => {
   operator.addEventListener("click", () => {
     //after each operator, store first number
+    // firstNum = number;
     //take operator value
+    symbol = operator.textContent;
+    display.textContent += symbol;
     //if second press, return result
   });
 });
 
 //return result at fourth press
 //add event listener to equal, return result and reset display
+//add event listener for clear and delete
 
 function operate(firstNum, operator, secondNum) {
   switch (operator) {
